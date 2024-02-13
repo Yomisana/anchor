df0252ed8d6ea2fe7cc8300d570bd2e79d818a83
yomisana <sammywhy3236@gmail.com>
2024-02-14 06:25:00 +0800

    Update Electron window settings and add subtitle text rotation

--- main.js
+++ main.js
@@ -1,35 +1,44 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+// const path = require('path')
 
-function createWindow () {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Anchor",
+    icon: "public/logo512.png",
+    autoHideMenuBar: true,
+    frame: true,
+    titleBarStyle: "hiddenInset",
+    webPreferences: {
+      devTools: !app.isPackaged,
+      fullscreenBoolean: false,
+      fullscreenableBoolean: false,
+      simpleFullscreenBoolean: false,
+      nodeIntegration: true,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: __dirname + "/preload.js", // use a preload script
+    },
+  });
 
-
-    if (app.isPackaged) {
-        // Electron 正在生產環境中運行
-        // 產品階段直接讀取 React 打包好的
-        mainWindow.loadFile('./build/index.html');
-    } else {
-        // 開發階段直接與 React 連線
-        mainWindow.loadURL('http://localhost:3000/');
-        // 開啟 DevTools.
-        mainWindow.webContents.openDevTools()
-    }
+  if (app.isPackaged) {
+    // Electron 正在生產環境中運行
+    // 產品階段直接讀取 React 打包好的
+    mainWindow.loadFile("./build/index.html");
+  } else {
+    // 開發階段直接與 React 連線
+    mainWindow.loadURL("http://localhost:3000/");
+    // 開啟 DevTools.
+    mainWindow.webContents.openDevTools();
+  }
 }
 
 app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+  createWindow();
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [subtitle_text, setSubtitle_text] = useState("your project");
+  const [prevIndex, setPrevIndex] = useState(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const text_array = [
+        "your project",
+        "stable diffusion webui",
+        "switch python version",
+        "manger python",
+      ];
+
+      let index = Math.floor(Math.random() * text_array.length);
+
+      // 如果新的索引與前一次相同，就找到一個不同的索引
+      while (index === prevIndex) {
+        index = Math.floor(Math.random() * text_array.length);
+      }
+      // add timestamp
+      console.log(`subtitle tick active`);
+      // console.log(`now subtitle: ${text_array[index]}, num: ${index}`);
+      setSubtitle_text(text_array[index]);
+      setPrevIndex(index);
+    }, 5000);
+
+    return () => clearInterval(intervalId); // 清除定時器
+  }, [prevIndex]); // 當 prevIndex 改變時重新執行
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      {/* Title */}
+      <h1 style={styles.title}>Anchor</h1>
+      {/* span easy using protable python some description */}
+      <div
+        style={{
+          ...styles.subtitle,
+          wordWrap: "break-word",
+        }}
+      >
+        Easy using portable python for {subtitle_text}.
+      </div>
+      {/* h2 or span status about select python installed or not install */}
+      <h2 style={styles.status}>Status: ✅ Installed | ❌ Not Installed</h2>
+      {/* Choose python version */}
+      <select style={styles.select}>
+        <option value="3.8">Python 3.8</option>
+        <option value="3.9">Python 3.9</option>
+      </select>
+      {/* Open default Terminal app (set path about select installed portable python) */}
+      <div style={{ flexDirection: "row", alignSelf: "center" }}>
+        <button style={styles.button}>Open Terminal</button>
+        <button style={styles.button}>Download</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+const styles = {
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#36393f", // Discord 暗黑模式下的黑色背景色
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+  },
+  title: {
+    color: "white",
+    fontSize: "4rem",
+    textAlign: "center",
+    margin: "20px",
+  },
+  subtitle: {
+    color: "white",
+    fontSize: "1.5rem",
+    textAlign: "center",
+    margin: "20px",
+    flexDirection: "row",
+  },
+  status: {
+    color: "white",
+    fontSize: "2rem",
+    textAlign: "center",
+    margin: "20px",
+  },
+  select: {
+    width: "80%",
+    padding: "10px",
+    fontSize: "1rem",
+    alignSelf: "center",
+    margin: "20px",
+    borderRadius: "50px",
+  },
+  button: {
+    width: "100",
+    padding: "10px",
+    fontSize: "1rem",
+    alignSelf: "center",
+    borderRadius: "50px",
+    marginLeft: "10px",
+    marginRight: "10px",
+  },
+};
